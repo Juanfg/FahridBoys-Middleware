423dc12d762a3de618795e7924d8eea408d38817
AdrianR <isadriti@hotmail.com>
2018-04-14 15:56:45 -0500

    Connection improved

--- services/TwitterService.js
+++ services/TwitterService.js
@@ -15,8 +15,7 @@ module.exports = function () {
 
     var Twitter = new TwitterStream(keys, false);
     Twitter.stream('statuses/filter', {
-        track: 'credito, seguro',
-        location: '-117.12776,14.5388286402,-86.811982388,32.72083'
+        locations: '-117.12776,14.5388286402,-86.811982388,32.72083'
     });
 
     Twitter.on('connection success', function (uri) {
@@ -28,13 +27,34 @@ module.exports = function () {
     });
 
     Twitter.on('data', function (obj) {
-        var data = JSON.parse(obj).text
-        Axios.post("http://localhost:8080/tweets", 
-        querystring.stringify({ text: data })).then(function (response) {
-            console.log(response.data);
-        }).catch(function (error) {
-            console.log(error);
-        });
+        var tweet = JSON.parse(obj);
+        console.log(tweet.hasOwnProperty('retweet_status'));
+        if (tweet.hasOwnProperty('retweet_status')) {
+        } else {
+            if (tweet.coordinates) {
+                Axios.post("http://10.50.82.149:8080/tweets", querystring.stringify({
+                    text: tweet.text,
+                    lat: tweet.coordinates.coordinates[1],
+                    lon: tweet.coordinates.coordinates[0]
+                })).then(function (response) {
+                    console.log(response.data);
+                }).catch(function (error) {
+                    console.log(error);
+                });
+            } else {
+                var lon = (tweet.place.bounding_box.coordinates[0][0][0] + tweet.place.bounding_box.coordinates[0][2][0])/2;
+                var lat = (tweet.place.bounding_box.coordinates[0][0][1] + tweet.place.bounding_box.coordinates[0][2][1])/2;
+                Axios.post("http://10.50.82.149:8080/tweets", querystring.stringify({
+                    text: tweet.text,
+                    lat: lat,
+                    lon: lon
+                })).then(function (response) {
+                    console.log(response.data);
+                }).catch(function (error) {
+                    console.log(error);
+                });
+            }
+        }
     });
 
     Twitter.pipe(fs.createWriteStream('tweets.json'));
